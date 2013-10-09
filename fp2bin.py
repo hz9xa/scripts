66cf813f3aeb154d7da08900bb7074a676488d6a
Eric <hz9xa@virginia.edu>
2013-10-08 23:05:23 -0400

    modified fp2bin.py

--- fp2bin.py
+++ fp2bin.py
@@ -1,11 +1,5 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
 
-This temporary script file is located here:
-C:\Users\Eric\.spyder2\.temp.py
-"""
-import sys,struct
+import struct
 def binary(num):
     return ''.join(bin(ord(c)).replace('0b', '').rjust(8, '0')\
     for c in struct.pack('!f', num))
@@ -15,6 +9,7 @@ def fp2bin(a):
     return binary(a)
 
 
-if "__name__" == "__main__":
+if __name__ == "__main__":
     while(True):
-        print("%s" % fp2bin(input("Floating number:")))
+        fp = input("Floating number:")
+        print("%s" % fp2bin(fp))
