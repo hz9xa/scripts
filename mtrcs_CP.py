388f6369e03f9436cd8ea15447b9a0a6fb428b3e
hang <hang@panda.(none)>
2014-01-26 21:52:01 -0500

    updated metrics script for CP

--- mtrcs_CP.py
+++ mtrcs_CP.py
@@ -1,5 +1,23 @@
-import os,sys
+import sys
+import math
 import numpy as np
+from hellinger import *
+
+def MSE(p,q):
+    if len(p) != len(q) :
+        print("ERROR: unequal length in mse");
+        sys.exit(1)
+    s = 0
+    N = len(p)
+    for (x,y) in zip(p,q):
+        s += (x-y)*(x-y)
+    return s/N
+def test(p,name):
+    for x in p:
+        try:
+            r = math.sqrt(x)
+        except:
+            print("%s %f" % (name,x))
 
 if __name__=='__main__':
     refile = open('ref.txt','r')
@@ -12,6 +30,7 @@ if __name__=='__main__':
     maxerr = 0
     minerr = 0
     stddev = 0    
+    h1 = 0; h2 = 0; h3 = 0;
 
     refnums = []
     imprenums = []
@@ -36,9 +55,21 @@ if __name__=='__main__':
     errarry = np.array(errs)
     meanerr = np.mean(errarry)
     mae = np.mean(np.absolute(errarry))
+    mse = MSE(refnums,imprenums)
     std = np.std(errarry)
+    
+    h1=hellinger1(np.absolute(np.array(refnums)),np.absolute(np.array(imprenums)));
+    h2=hellinger2(np.absolute(np.array(refnums)),np.absolute(np.array(imprenums)));
+    h3=hellinger3(np.absolute(np.array(refnums)),np.absolute(np.array(imprenums)));
+#    test(refnums,'ref');
+#    test(imprenums,'impre');
+
     print("="*40)
     print("mean error: %f" % meanerr)
     print("MAE: %f" % mae)
+    print("mse: %f" % mse)
     print("std: %f" % std)
+    print("h1: %f" % h1)
+    print("h2: %f" % h1)
+    print("h3: %f" % h1)
     print("="*40)
---
+++ plt_cp_designspace.py
@@ -0,0 +1,6 @@
+import sys
+import matplotlib.pyplot as plt
+# (power,quality) 20,19,18,10
+dsgn_pt_wrt_mean_err=[(2.460271,0.111905),(2.843137,0.042387),(3.588332,0.030561),(4.075951,0.000072)]
+
+
