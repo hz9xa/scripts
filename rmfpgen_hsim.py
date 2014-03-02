a500ee341ff71e58d03b9b3b3cb27fa09e276093
Hang <hz9xa@gmail.com>
2014-03-02 11:07:17 -0500

    added bit truncation for rmfpgen_hsim

--- rmfpgen_hsim.py
+++ rmfpgen_hsim.py
@@ -11,6 +11,7 @@ period=1.7
 numVecs=1000 #number of input vectors for each input
 inputs=["X","Y"]
 inputWidth=32 #LSb is bit 0
+c=0
 outfile="./output/hsim.vec"
 ######################
 vecs=[]
@@ -29,7 +30,10 @@ with open(outfile,"w") as of:
     for input in inputs:
         index = inputs.index(input)
         for i in range(inputWidth):
-            of.write(input+"<"+str(i)+"> ")
+            of.write(input+"<"+str(i)+"> ")                
             for j in range(index*numVecs,(index+1)*numVecs):
-                of.write(vecs[j][inputWidth-i-1])
+                if i < c:
+                    of.write("0")
+                else:
+                    of.write(vecs[j][inputWidth-i-1])
             of.write("\n")
